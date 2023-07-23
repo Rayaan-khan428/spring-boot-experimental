6aaeb9c414dafc218cbd644cbf1febffde512861
Rayaan Khan <rayaan.k.ca@gmail.com>
2023-07-23 14:27:19 -0400

    added yml file

---
+++ src/main/java/com/rayaan/Main.java
@@ -0,0 +1,13 @@
+package com.rayaan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // specifies that this is a springtboot application
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
