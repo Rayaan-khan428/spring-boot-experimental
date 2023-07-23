90bfebedd837fb3f3771a1d6ade352eb8c7c9475
Rayaan Khan <rayaan.k.ca@gmail.com>
2023-07-23 14:36:03 -0400

    added first ever api endpoint

--- src/main/java/com/rayaan/Main.java
+++ src/main/java/com/rayaan/Main.java
@@ -2,12 +2,20 @@ package com.rayaan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication // specifies that this is a springtboot application
+@RestController // required for API endpoint
 public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping("/") // allows users to use as Rest endpoint
+    public String greet() {
+        return "Hello";
     }
 
 }
